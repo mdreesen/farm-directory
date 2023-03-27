cc5b0c1a91a4d87331a4a92d22cd079c18acf27d
Matt Mannion <mattpmannion@gmail.com>
2023-03-27 05:52:09 -0400

    compressed ax and fixed deleteOne

--- lib/axios.lib.ts
+++ lib/axios.lib.ts
@@ -5,28 +5,14 @@ import { cfg } from 'settings/cfg.settings';
 export const ax = {
   farmers: {
     find: axios
-      .post(
-        cfg.api.find,
-        {
-          ...cfg.api.settings.body,
-          sort: { postedAt: -1 },
-        },
-        {
-          headers: cfg.api.settings.headers,
-        }
-      )
+      .post(cfg.api.find, { ...cfg.api.settings.body, sort: { postedAt: -1 } }, { headers: cfg.api.settings.headers })
       .then((res) => res.data),
     insertOne: (req: NextApiRequest) =>
       axios
         .post(
           cfg.api.insertOne,
-          {
-            ...cfg.api.settings.body,
-            document: req.body,
-          },
-          {
-            headers: cfg.api.settings.headers,
-          }
+          { ...cfg.api.settings.body, document: req.body },
+          { headers: cfg.api.settings.headers }
         )
         .then((res) => res.data),
     updateOne: (req: NextApiRequest) =>
@@ -42,21 +28,16 @@ export const ax = {
               },
             },
           },
-          {
-            headers: cfg.api.settings.headers,
-          }
+          { headers: cfg.api.settings.headers }
         )
         .then((res) => res.data),
     deleteOne: (req: NextApiRequest) =>
-      axios.post(
-        cfg.api.deleteOne,
-        {
-          ...cfg.api.settings.body,
-          filter: { _id: { $oid: req.body._id } },
-        },
-        {
-          headers: cfg.api.settings.headers,
-        }
-      ),
+      axios
+        .post(
+          cfg.api.deleteOne,
+          { ...cfg.api.settings.body, filter: { _id: { $oid: req.body._id } } },
+          { headers: cfg.api.settings.headers }
+        )
+        .then((res) => res.data),
   },
 };
