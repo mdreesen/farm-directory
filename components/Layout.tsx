997dff02015637a602331edfcfbba742575dde1c
Matt Mannion <mattpmannion@gmail.com>
2023-03-27 23:30:05 -0400

    removed footer ref

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
 import { Navigation } from 'components/Navigation';
 import { ReactNode } from 'react';
-// import Footer from './Footer';
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navigation />
       <main>{children}</main>
-      {/* <Footer /> */}
     </>
   );
 }
