55f1f402d76b4e6aa577a5fea46d5f8c5a7b7784
Matt Mannion <mattpmannion@gmail.com>
2023-04-02 16:59:39 -0400

    fixed footer path i missed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
+import { Footer } from 'components/Footer';
 import { Navigation } from 'components/Navigation';
 import { ReactNode } from 'react';
-import { Footer } from './Footer';
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
