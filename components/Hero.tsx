023718f1afe875c2f61249981f7a1556866838dc
Michael Dreesen <michaeldreesen90@gmail.com>
2023-07-11 20:27:26 -0600

    Changing home screen for mobile. TODO: need to work on admin sign up

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,6 +13,19 @@ export function Hero({
   source: string;
   imageTitle?: string;
 }) {
+
+  const downArrow = (
+<div className={styles["center-con"]}>
+    
+    <div className={styles["round"]}>
+        <span className={styles['arrowSpan']} />
+        <span className={styles['arrowSpan']} />
+        <span className={styles['arrowSpan']} />
+        <span className={styles['arrowSpan']} />
+    </div>
+    
+</div>
+  );
   return (
     <div>
       {video && (
@@ -20,7 +33,11 @@ export function Hero({
           <video autoPlay width='250' loop muted className={styles['hero']}>
             <source src={source} type='video/mp4' />
           </video>
-          <h1 className={styles['title']}>Farm Directory</h1>
+          <h1 className={styles['title']}>
+            Farm Directory
+
+            {downArrow}
+            </h1>
         </div>
       )}
       {image && (
--- components/NoFarmer.tsx
+++ components/NoFarmer.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import styles from '../styles/NoFarmer.module.css';
 
 export function NoFarmer() {
-    return <span className={styles['no-data']}>Apologies, No Farmer For This Category.</span>
+    return (
+        <div>
+            <span className={styles['no-data']}>Apologies, No Farmer For This Category.</span>
+            <span className={styles['no-data']}> If you are a farmer, you could be the first for this category!</span>
+            <span className={styles['no-data']}> If you would want to sell your products, contact us and we will help you through the process.</span>
+        </div>
+    )
 };
