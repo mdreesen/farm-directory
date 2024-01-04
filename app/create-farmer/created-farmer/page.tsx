cd4a3973c2a73d052002f727f7d239da70195831
Michael Dreesen <michaeldreesen90@gmail.com>
2024-01-04 15:04:34 -0700

    Updated created-farmer page CSS

--- app/create-farmer/created-farmer/page.tsx
+++ app/create-farmer/created-farmer/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import styles from "@/app/styles/CreateFarmer.module.css";
 
 export default function Page() {
 
   return (
-    <div>
+    <div className={styles['created']}>
       <span>Farmer has been created!</span>
       <span>Thank You!</span>
 
-      <Link href="/create-farmer/2c65d499cc0fe715a460d0c16eb9f65be7972c35">Back To Creating</Link>
+      <Link href="/create-farmer/2c65d499cc0fe715a460d0c16eb9f65be7972c35">Click to Create A Farmer</Link>
     </div>
   )
 }
