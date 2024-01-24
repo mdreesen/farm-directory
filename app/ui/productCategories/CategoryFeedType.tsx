a196d34551aaf6504132e4b99da069369cb6af09
Michael Dreesen <michaeldreesen90@gmail.com>
2024-01-24 10:28:53 -0700

    Updated feed category dropdown

--- app/ui/productCategories/CategoryFeedType.tsx
+++ app/ui/productCategories/CategoryFeedType.tsx
@@ -4,9 +4,9 @@ export function CategoryFeedType() {
   return (
     <>
       <option value={""}>Please Select Feed Type</option>
-      <option value="Grain">Grain</option>
-      <option value="Grass">Grass</option>
-      <option value="Grain/Grass">Grain & Grass</option>
+      <option value="Grain">Grain Finished</option>
+      <option value="Grass">Grass Finished</option>
+      <option value="Grain/Grass">Grain & Grass Finished</option>
     </>
   );
 }
