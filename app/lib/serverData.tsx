d01391ed1a14480cbf960870d9f9794681808787
Michael Dreesen <michaeldreesen90@gmail.com>
2024-02-18 20:43:23 -0700

    restore auth

--- app/lib/serverData.tsx
+++ app/lib/serverData.tsx
@@ -1,13 +1,7 @@
 'use server';
 import { cookies } from 'next/headers'
-
-
-// export async function deleteToken() {
-//     return cookies().delete(`${process.env.COOKIE_KEY}`);
-// };
   
 export async function deleteToken() {
-    // const data = cookiesList.get(`${process.env.COOKIE_KEY}`);
     const deleteCookie = cookies().delete(`${process.env.COOKIE_KEY}`);
 
     return deleteCookie
