dc44126c40081f3a33afa637961020b4936674cc
Michael Dreesen <michaeldreesen90@gmail.com>
2024-03-07 20:14:27 -0700

    Updating farmer button

--- app/ui/buttons/farmerUpdate.tsx
+++ app/ui/buttons/farmerUpdate.tsx
@@ -5,7 +5,7 @@ import { loggedInUserData } from '@/app/lib/cookieData';
 export default async function FarmerUpdate(data: any) {
     const auth = await loggedInUserData();
 
-    const showUpdateButton = data?.email === auth?.email && auth?.isFarmer;
+    const showUpdateButton = data?.data?.email === auth?.email && auth?.isFarmer;
 
-    return showUpdateButton && <div className={styles['update']}><a href={`/profile-farmer/${data?.data}/update`} className={styles['update-link']}>Update Information</a></div>
+    return showUpdateButton && <div className={styles['update']}><a href={`/profile-farmer/${data?.data?._id}/update`} className={styles['update-link']}>Update Information</a></div>
 };
