7c677ba341d2c0d91a40767edfcdffd9bb4df43a
Michael Dreesen <michaeldreesen90@gmail.com>
2024-04-07 20:41:52 -0600

    Got it

--- app/api/Farmers/[id]/products/route.ts
+++ app/api/Farmers/[id]/products/route.ts
@@ -5,38 +5,15 @@ import { NextResponse } from 'next/server';
 // https://www.youtube.com/watch?v=XRXjJRJ03_A&ab_channel=MongoDB
 
 // Get Single Farmer By ID
-export async function GET(req: any, { params }: any) {
-    try {
-        const farmer = await Farmer.find({});
-        return NextResponse.json({ farmer }, { status: 200 });
-
-    } catch (error) {
-        return NextResponse.json({ message: "Error", error }, { status: 500 })
-    }
-};
-
-// Get Single Farmer By ID
 export async function PUT(req: any, { params }: any) {
     try {
         const { id } = params;
         const body = await req.json();
         const productData = body?.formData;
 
-        const farmer = await Farmer.updateMany(id, {$addToSet: {products: productData} });
+        const farmer = await Farmer.findOneAndUpdate({ _id: id }, { $push: { products: productData } }, { new: true });
 
         return NextResponse.json({ message: "Products Updated" }, { status: 200 });
-
-    } catch (error) {
-        return NextResponse.json({ message: "Error", error }, { status: 500 })
-    }
-};
-
-export async function DELETE(req: any, { params }: any) {
-    try {
-        const { id } = params;
-        await Farmer.findByIdAndDelete(id);
-
-        return NextResponse.json({ message: "Farmer Deleted" }, { status: 200 });
 
     } catch (error) {
         return NextResponse.json({ message: "Error", error }, { status: 500 })
