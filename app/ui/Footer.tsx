610aab91ee7dd5af3eb12823bb11cf9f8d96a8d9
Michael Dreesen <michaeldreesen90@gmail.com>
2024-04-24 15:17:06 -0600

    Editing footer to take out anchor tag for logo

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -5,54 +5,19 @@ import { currentYear } from '../lib/date';
 
 export function Footer() {
 
-  const socialLinks = (
-    <>
-      <div className='flex flex-col gap-2'>
-        <span>Find Us On</span>
-        <div className='flex justify-around'>
-          <a className="hover:text-yellow-500" aria-label="Visit TrendyMinds Facebook" href="https://www.facebook.com/thefarmdirectory" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="h-3">
-              <path fill="currentColor" d="m279.14 288 14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z">
-              </path>
-            </svg>
-          </a>
-          <a className="hover:text-yellow-500" aria-label="Visit TrendyMinds Instagram" href="https://www.instagram.com/thefarmdirectory/?igsh=M2F5dW1iaDA3Ym9u" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="h-3">
-              <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z">
-              </path>
-            </svg>
-          </a>
-        </div>
-      </div>
-    </>
-  );
-
-  const routingLinks = (
-    <div>
-      <div className='flex flex-col items-center'>
-        <Link href="/about">About Us</Link>
-        <Link href="/contact-us">Contact Us</Link>
-        <Link href="/terms-conditions">Terms & Conditions</Link>
-      </div>
-
-    </div>
-  );
-
   return (
     <footer className="bg-white dark:bg-gray-900">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
-              <Image
-                priority={true}
-                className="w-100 h-100 rounded-full mr-4"
-                src="/images/logos/logo.webp"
-                width={154}
-                height={66}
-                alt="Picture of barn and silo with The Farm Directory.com text"
-              />
-            </a>
+            <Image
+              priority={true}
+              className="w-100 h-100 rounded-full mr-4"
+              src="/images/logos/logo.webp"
+              width={154}
+              height={66}
+              alt="Picture of barn and silo with The Farm Directory.com text"
+            />
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
