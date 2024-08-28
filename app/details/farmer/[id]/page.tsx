5b8f905bc6a2b2582c06abf482351fb88bb4ea8c
Michael Dreesen <michaeldreesen90@gmail.com>
2024-08-28 11:16:07 -0600

    feature(Updating farmer details)

--- app/details/farmer/[id]/page.tsx
+++ app/details/farmer/[id]/page.tsx
@@ -1,11 +1,11 @@
 import { useParams } from 'next/navigation';
-import {fetchSingleFarmerById} from '@/actions/farmer';
+import { fetchSingleFarmerById } from '@/actions/farmer';
 
 export default async function page({ params }: { params: { id: string } }) {
   const id = params.id;
-console.log(id);
-const farmer = await fetchSingleFarmerById(id) as any;
-console.log(farmer)
+  console.log(id);
+  const farmer = await fetchSingleFarmerById(id) as any;
+  console.log(farmer)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
@@ -21,13 +21,21 @@ console.log(farmer)
             </div>
             <ul
               role="list"
-              className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
+              className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
             >
               {farmer.products.map((item: any) => (
                 <li key={item._id}>
                   <img alt="" src={'/images/products/beef.webp'} className="mx-auto h-24 w-24 rounded-full" />
-                  <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{item.product_title}</h3>
-                  <p className="text-sm leading-6 text-gray-600">{item.product_description}</p>
+                  <h3 className="mt-6 text-lg font-semibold leading-7 tracking-tight text-gray-900">
+                    {item.product_title}
+                  </h3>
+
+                  <p className="pt-2 text-base leading-6 text-gray-600">{item.product_description}</p>
+
+                  <p className="pt-2 text-base leading-7 tracking-tight text-gray-900 flex flex-col">
+                    <span>Product Availability</span>
+                    <span>{item.product_available}</span>
+                  </p>
                 </li>
               ))}
             </ul>
