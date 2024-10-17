5c45520d5754eb3c326fa65ee990e9f205a24744
Michael Dreesen <michaeldreesen90@gmail.com>
2024-10-17 11:15:35 -0600

    feature(New Join email list)

--- app/email-list/page.tsx
+++ app/email-list/page.tsx
@@ -1,19 +1,14 @@
+'use client'
 import React from 'react';
 import styles from '@/app/styles/contact/Contact.module.css';
 import { ContactUs } from '@/app/ui/forms/ContactUs';
 import { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'Contact Us',
-  description: 'Contact us Farm Directory',
-}
-
-
 export default function Page() {
 
   return (
       <div className={styles.main}>
-        <ContactUs />
+        <div className="gzform-embed-popup-a3c0d52d-0c1a-4227-83bb-f016de878aca" />
       </div>
   );
 }
--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -20,11 +20,17 @@ export function Footer() {
             />
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-          <div>
+            <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white text-start">About</h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li>
                   <a href="/about" className="hover:underline text-gray-900 dark:text-white">About Us</a>
+                </li>  
+                <li>
+                  <a href="/email-list" className="hover:underline text-gray-900 dark:text-white">Join our email list</a>
+                </li>              
+                <li>
+                  <a href="/contact-us" className="hover:underline text-gray-900 dark:text-white">Contact Us</a>
                 </li>
               </ul>
             </div>
