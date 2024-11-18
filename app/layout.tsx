8abb6d7b4b3aa6e2e675a510dd542e9cd67035e8
Michael Dreesen <mdreesen@Mac.lan>
2024-11-18 13:38:47 -0700

    feature(Adding analytics and speed insights)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Provider } from "./provider";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // Import components for overall pages
 import Navigation from '@/ui/navigation/Navigation';
@@ -24,6 +26,9 @@ export default function RootLayout({
         <body className={inter.className}>
           <Navigation />
           {children}
+          <Analytics />
+          <SpeedInsights />
+          <script id="gozen-main" data-app_id="c8f8eac6-702a-4fac-af7f-c7e6e0a7a708" src="https://signup-forms-cdn.app.gozen.io/v1/gozen-main.js" type="text/javascript" defer async />
         </body>
       </Provider>
     </html>
