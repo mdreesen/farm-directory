a2701a806da3c1d6d8e8988b833bc6d9e8eac987
Michael Dreesen <mdreesen@Mac.lan>
2024-11-19 13:05:26 -0700

    feature(Changing verifying email pages so it does not become to redundant)

--- app/authentication/verification/[token]/verified/page.tsx
+++ app/authentication/verification/[token]/verified/page.tsx
@@ -6,10 +6,9 @@ export default function Page() {
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black">
-                        Thank you for verifying your email
+                        You are now verified!
                     </h2>
-                    <p>You are now verified!</p><br/>
-                    <p>You can log into your account and get started with adding your information and products!</p>
+                    <p>You can get started with adding your information and products!</p>
                 </div>
             </div>
         </div>
