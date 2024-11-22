871bdfbda3489906e677386b5072638c7b7d186b
Michael Dreesen <mdreesen@Mac.lan>
2024-11-22 10:27:59 -0700

    feature(testing out google analytica)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Provider } from "./provider";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import Script from "next/script";
 // Import components for overall pages
 import Navigation from '@/ui/navigation/Navigation';
 
@@ -26,9 +26,31 @@ export default function RootLayout({
         <body className={inter.className}>
           <Navigation />
           {children}
+
+          {/* Vercel Analytics */}
           <Analytics />
+
+          {/* Vercel Speed insights */}
           <SpeedInsights />
+
+          {/* Google Analytics */}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-MQ18H2MPZ1"></Script>
+          <Script id="google-analytics">
+            {
+              `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-MQ18H2MPZ1');
+              `
+            }
+
+          </Script>
+
+          {/* Gozen Email Popup */}
           <script id="gozen-main" data-app_id="c8f8eac6-702a-4fac-af7f-c7e6e0a7a708" src="https://signup-forms-cdn.app.gozen.io/v1/gozen-main.js" type="text/javascript" defer async />
+
         </body>
       </Provider>
     </html>
