0aa85d3857517ad5ed198720799c9541c1346c04
Michael Dreesen <mdreesen@Mac.lan>
2024-11-22 14:22:30 -0700

    feature(Taking out the onClick for the login button)

--- app/authentication/login/page.tsx
+++ app/authentication/login/page.tsx
@@ -80,7 +80,6 @@ export default function Page() {
                             <button
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-[#7A3A30] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#af8882] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                                onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })}
                             >
                                 Sign in
                             </button>
